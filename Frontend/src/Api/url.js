9cc2b2a8c5c4ec983a9dc16f3e54be7e294bd4ec
Sriman Kundu <srimankundu9@gmail.com>
2026-07-24 20:39:28 +0530

    Update url.js

--- Frontend/src/Api/url.js
+++ Frontend/src/Api/url.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
-const BACKEND_URL = axios.create({
-  baseURL: "https://my-notes-jafj.onrender.com",
+const api = axios.create({
+  baseURL: "https://my-notes-jafj.onrender.com/api/v1/noteapp/", // Use this if your backend routes require this prefix
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-export default BACKEND_URL;
+export default api;
